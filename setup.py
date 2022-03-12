b2dc150aad801b5ce874e88841a2ca03eff132aa
Florian <f.aumeier@me.com>
2022-03-12 19:09:44 +0100

    chore: bump version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ README = (HERE / 'README.md').read_text()
 
 setup(
     name='get-repo',
-    version='0.1.0',
+    version='0.1.1',
     description='Small cli utility for cloning git repos in an orderly manner',
     long_description=README,
     long_description_content_type='text/markdown',
